7efe1fb26cc06621ef997b4876eed04cbfeeda67
Lucio Yen <U202213143@upc.edu.pe>
2024-11-14 01:52:55 -0500

    feat: add AppBar and home screen route

--- roademics/lib/main.dart
+++ roademics/lib/main.dart
@@ -1,20 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:roademics/shared/presentation/pages/home_page.dart';
+void main() => runApp(MyApp());
 
-void main() {
-  runApp(const MainApp());
-}
-
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return MaterialApp(
+      title: 'Flutter App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
+      home: HomePage(), // Invoca HomePage aquí
     );
   }
 }
