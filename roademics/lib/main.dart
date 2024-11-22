5989f9b6ebe70bcd837302e378770054cce321d8
Lucio Yen <U202213143@upc.edu.pe>
2024-11-22 05:02:08 -0500

    feat: add welcome page & improved design

--- roademics/lib/main.dart
+++ roademics/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:roademics/presentation/authentication/bloc/login_bloc.dart';
 import 'package:roademics/presentation/authentication/pages/login_page.dart';
+import 'package:roademics/presentation/authentication/pages/welcome_page.dart';
 
 void main() {
   runApp(const MainApp());
@@ -13,11 +14,25 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocProvider<LoginBloc>(
-        create: (context) => LoginBloc(),
-        child: MaterialApp(
-          darkTheme: ThemeData.dark(),
-          theme: ThemeData.light(),
-          home: const LoginPage(),
-        ));
+      create: (context) => LoginBloc(),
+      child: MaterialApp(
+        darkTheme: ThemeData.dark(),
+        theme: ThemeData(
+          // Configuración del tema principal con la fuente Raleway
+          fontFamily: 'Raleway',
+          textTheme: const TextTheme(
+          ),
+          elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ElevatedButton.styleFrom(
+              textStyle: const TextStyle(fontFamily: 'Raleway'),
+            ),
+          ),
+        ),
+        home: const WelcomePage(),
+        routes: {
+          '/login': (context) => const LoginPage(),
+        },
+      ),
+    );
   }
 }
--- roademics/lib/presentation/authentication/pages/login_page.dart
+++ roademics/lib/presentation/authentication/pages/login_page.dart
@@ -25,112 +25,171 @@ class _LoginPageState extends State<LoginPage> {
     return BlocProvider<PasswordHiddenCubit>(
       create: (context) => PasswordHiddenCubit(),
       child: Scaffold(
-        body: SafeArea(
-          child: BlocListener<LoginBloc, LoginState>(
-            listener: (context, state) {
-              if (state is LoginSuccess) {
-                ScaffoldMessenger.of(context).showSnackBar(
-                  SnackBar(content: Text('Welcome: ${state.user.username}')),
-                );
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Color(0xFF0A3D62), Color(0xFF40E0D0)], // MediumTurquoise y Turquoise
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+            ),
+          ),
+          child: SafeArea(
+            child: BlocListener<LoginBloc, LoginState>(
+              listener: (context, state) {
+                if (state is LoginSuccess) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(content: Text('Welcome: ${state.user.username}')),
+                  );
 
-                Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const HomePage(),
-                  ),
-                );
-              } else if (state is LoginError) {
-                ScaffoldMessenger.of(context).showSnackBar(
-                  SnackBar(content: Text(state.message)),
-                );
-              }
-            },
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Text(
-                    "Roademics",
-                    style: TextStyle(
-                      fontSize: 32,
-                      fontWeight: FontWeight.bold,
-                      color: Color(0xFF0A3D62),
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const HomePage(),
+                    ),
+                  );
+                } else if (state is LoginError) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(content: Text(state.message)),
+                  );
+                }
+              },
+              child: Center(
+                child: Container(
+                  padding: const EdgeInsets.all(16.0),
+                  margin: const EdgeInsets.symmetric(horizontal: 24.0),
+                  decoration: BoxDecoration(
+                    color: Colors.white.withOpacity(0.2), // Fondo semitransparente
+                    borderRadius: BorderRadius.circular(16.0), // Bordes redondeados
+                    border: Border.all(
+                      color: Colors.white.withOpacity(0.5), // Borde blanco semitransparente
+                      width: 1.5,
                     ),
                   ),
-                  const SizedBox(height: 32),
-                  TextField(
-                    controller: _usernameController,
-                    decoration: const InputDecoration(
-                      prefixIcon: Icon(Icons.person),
-                      border: OutlineInputBorder(),
-                      labelText: 'Correo electrónico o usuario',
-                    ),
-                  ),
-                  const SizedBox(height: 16.0),
-                  BlocBuilder<PasswordHiddenCubit, bool>(
-                    builder: (context, isPasswordHidden) {
-                      return TextField(
-                        obscureText: isPasswordHidden,
-                        controller: _passwordController,
-                        decoration: InputDecoration(
-                          prefixIcon: const Icon(Icons.lock),
-                          border: const OutlineInputBorder(),
-                          labelText: 'Contraseña',
-                          suffixIcon: IconButton(
-                            onPressed: () => context
-                                .read<PasswordHiddenCubit>()
-                                .togglePasswordVisibility(),
-                            icon: Icon(
-                              isPasswordHidden
-                                  ? Icons.visibility_off
-                                  : Icons.visibility,
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      const Text(
+                        "Login",
+                        style: TextStyle(
+                          fontSize: 32,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                      const SizedBox(height: 32),
+                      TextField(
+                        controller: _usernameController,
+                        decoration: const InputDecoration(
+                          prefixIcon: Icon(Icons.email, color: Colors.white),
+                          enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.white, width: 1.5),
+                          ),
+                          focusedBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.white, width: 2.0),
+                          ),
+                          labelText: 'Email',
+                          labelStyle: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+                        ),
+                        style: const TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+                      ),
+                      const SizedBox(height: 16.0),
+                      BlocBuilder<PasswordHiddenCubit, bool>(
+                        builder: (context, isPasswordHidden) {
+                          return TextField(
+                            obscureText: isPasswordHidden,
+                            controller: _passwordController,
+                            decoration: InputDecoration(
+                              prefixIcon: const Icon(Icons.lock, color: Colors.white),
+                              enabledBorder: const UnderlineInputBorder(
+                                borderSide: BorderSide(color: Colors.white, width: 1.5),
+                              ),
+                              focusedBorder: const UnderlineInputBorder(
+                                borderSide: BorderSide(color: Colors.white, width: 2.0),
+                              ),
+                              labelText: 'Password',
+                              labelStyle: const TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+                              suffixIcon: IconButton(
+                                onPressed: () => context
+                                    .read<PasswordHiddenCubit>()
+                                    .togglePasswordVisibility(),
+                                icon: Icon(
+                                  isPasswordHidden
+                                      ? Icons.visibility_off
+                                      : Icons.visibility,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ),
+                            style: const TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+                          );
+                        },
+                      ),
+                      const SizedBox(height: 8.0),
+                      Align(
+                        alignment: Alignment.centerLeft,
+                        child: TextButton(
+                          onPressed: () {
+                            // Acción para "Forgot my password"
+                          },
+                          child: const Text(
+                            'Forgot my password',
+                            style: TextStyle(fontSize: 16, color: Colors.white, fontWeight: FontWeight.bold), // Itálica
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 16.0),
+                      SizedBox(
+                        width: double.infinity,
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: const Color.fromARGB(255, 9, 50, 75), // Color del botón
+                            foregroundColor: Colors.white,
+                            padding: const EdgeInsets.symmetric(vertical: 16.0),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(12.0),
                             ),
                           ),
+                          onPressed: () {
+                            final String username = _usernameController.text;
+                            final String password = _passwordController.text;
+                            context.read<LoginBloc>().add(
+                                  LoginSubmitted(
+                                    username: username,
+                                    password: password,
+                                  ),
+                                );
+                          },
+                          child: const Text('Login', style: TextStyle(color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold),),
                         ),
-                      );
-                    },
-                  ),
-                  const SizedBox(height: 16.0),
-                  SizedBox(
-                    width: double.infinity,
-                    child: ElevatedButton(
-                      onPressed: () {
-                        final String username = _usernameController.text;
-                        final String password = _passwordController.text;
-                        context.read<LoginBloc>().add(
-                              LoginSubmitted(
-                                username: username,
-                                password: password,
+                      ),
+                      const SizedBox(height: 16.0),
+                      TextButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => MultiBlocProvider(
+                                providers: [
+                                  BlocProvider<SignupBloc>(
+                                    create: (_) => SignupBloc(),
+                                  ),
+                                  BlocProvider<ProfileBloc>(
+                                    create: (_) => ProfileBloc(),
+                                  ),
+                                ],
+                                child: const SignUpFlowPage(),
                               ),
-                            );
-                      },
-                      child: const Text('Log In'),
-                    ),
-                  ),
-                  const SizedBox(height: 8.0),
-                  TextButton(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => MultiBlocProvider(
-                            providers: [
-                              BlocProvider<SignupBloc>(
-                                create: (_) => SignupBloc(),
-                              ),
-                              BlocProvider<ProfileBloc>(
-                                create: (_) => ProfileBloc(),
-                              ),
-                            ],
-                            child: const SignUpFlowPage(),
-                          ),
+                            ),
+                          );
+                        },
+                        child: const Text(
+                          'New member? Create an account',
+                          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
                         ),
-                      );
-                    },
-                    child: const Text('Sign Up'),
+                      ),
+                    ],
                   ),
-                ],
+                ),
               ),
             ),
           ),
